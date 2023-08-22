a74af0e89cabf9dd928fb35a0f3985c13389e91c
NguyenThanhNhan2507 <65636315+NguyenThanhNhan2507@users.noreply.github.com>
2023-08-22 12:02:03 +0700

    feat: get all workspace (#5)

--- src/resources/workspace/workspace.service.ts
+++ src/resources/workspace/workspace.service.ts
@@ -1,15 +1,21 @@
 import { Injectable } from '@nestjs/common';
 import { CreateWorkspaceDto } from './dto/create-workspace.dto';
 import { UpdateWorkspaceDto } from './dto/update-workspace.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Workspace } from './entities/workspace.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class WorkspaceService {
+	constructor(
+		@InjectRepository(Workspace) private workspaceRepos: Repository<Workspace>
+	) {}
 	create(createWorkspaceDto: CreateWorkspaceDto) {
 		return 'This action adds a new workspace';
 	}
 
-	findAll() {
-		return `This action returns all workspace`;
+	async findAll() {
+		return await this.workspaceRepos.find();
 	}
 
 	findOne(id: number) {
