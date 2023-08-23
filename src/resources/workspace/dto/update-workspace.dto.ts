dd46ff31694fbd963a0834c9d1bbabc0e18b7db2
demosofa <62540039+demosofa@users.noreply.github.com>
2023-08-23 12:10:33 +0700

    feat: accept/reject workspace (#16)

--- src/resources/workspace/dto/update-workspace.dto.ts
+++ src/resources/workspace/dto/update-workspace.dto.ts
@@ -1,12 +1,12 @@
 import { WORKSPACE_STATUS } from '@common/enums';
-import { IsEnum, IsNotEmpty, IsOptional } from 'class-validator';
+import { IsEnum, IsOptional, IsString } from 'class-validator';
 
 export class UpdateWorkspaceDto {
 	@IsOptional()
-	@IsNotEmpty()
-	title_workspace: string;
+	@IsString()
+	title_workspace?: string;
 
 	@IsOptional()
 	@IsEnum(WORKSPACE_STATUS)
-	status: WORKSPACE_STATUS;
+	status?: WORKSPACE_STATUS;
 }
