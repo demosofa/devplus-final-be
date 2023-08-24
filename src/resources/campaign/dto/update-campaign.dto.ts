da3b3488b2ab5a3e301f214dc5d8a0270891a4c0
datnvn <idolsupperman3215@gmail.com>
2023-08-24 14:08:50 +0700

    feat: dp18 change update dto

--- src/resources/campaign/dto/update-campaign.dto.ts
+++ src/resources/campaign/dto/update-campaign.dto.ts
@@ -1,4 +1,8 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateCampaignDto } from './create-campaign.dto';
+import { IsEmpty } from 'class-validator';
 
-export class UpdateCampaignDto extends PartialType(CreateCampaignDto) {}
+export class UpdateCampaignDto extends PartialType(CreateCampaignDto) {
+	@IsEmpty()
+	workspaceId?: number;
+}
