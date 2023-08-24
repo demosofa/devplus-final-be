ffc2b536c44d58c88e3007853d5e86ce748f344c
Nguyen Thanh Nhan <nhandaica125@gmail.com>
2023-08-24 09:53:20 +0700

    feat: create campaign

--- src/resources/campaign/campaign.module.ts
+++ src/resources/campaign/campaign.module.ts
@@ -3,10 +3,22 @@ import { CampaignService } from './campaign.service';
 import { CampaignController } from './campaign.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Campaign } from './entities/campaign.entity';
+import { Workspace } from '@resources/workspace/entities/workspace.entity';
+import { WorkspaceModule } from '@resources/workspace/workspace.module';
+import { WorkspaceService } from '@resources/workspace/workspace.service';
+import { UserModule } from '@resources/user/user.module';
+import { RoleModule } from '@resources/role/role.module';
+import { User } from '@resources/user/entities/user.entity';
+import { Role } from '@resources/role/entities/role.entity';
 
 @Module({
-	imports: [TypeOrmModule.forFeature([Campaign])],
+	imports: [
+		TypeOrmModule.forFeature([Campaign, Workspace, Role, User]),
+		WorkspaceModule,
+		RoleModule,
+		UserModule,
+	],
 	controllers: [CampaignController],
-	providers: [CampaignService],
+	providers: [CampaignService, WorkspaceService],
 })
 export class CampaignModule {}
--- src/resources/campaign/dto/create-campaign.dto.ts
+++ src/resources/campaign/dto/create-campaign.dto.ts
@@ -1,1 +1,17 @@
-export class CreateCampaignDto {}
+import { Type } from 'class-transformer';
+import { IsNotEmpty } from 'class-validator';
+
+export class CreateCampaignDto {
+	@IsNotEmpty()
+	name: string;
+
+	@IsNotEmpty()
+	description: string;
+
+	@IsNotEmpty()
+	expired_time: string;
+
+	@IsNotEmpty()
+	@Type(() => Number)
+	workspaceId: number;
+}
