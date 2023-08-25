1230008657c32a13baaa5c16cd340262ae8614fb
VanThen60hz <nguyenvthang2409@gmail.com>
2023-08-25 10:36:11 +0700

    fix: pagination

--- src/common/pagination/common.utils.ts
+++
@@ -1,8 +0,0 @@
-export function calculatePagination(count: number, take: number, page: number) {
-	const totalPages = Math.ceil(count / take);
-	const currentPage = page;
-	return {
-		totalPages,
-		currentPage,
-	};
-}
