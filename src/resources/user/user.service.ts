bb73b32b879841d1d1ae97d6366a7bbb7ca0468a
demosofa <62540039+demosofa@users.noreply.github.com>
2023-08-29 17:27:23 +0700

    Feat/auth (#39)
    * fix: auth and user
    
    * fix: token not provide enough info
    
    * fix: authGuard to provide user entity instead of payload
    
    * fix: create user api not hash password
    
    * fix: create user api not hash password
    
    * fix: import bcrypt

--- src/resources/user/user.service.ts
+++ src/resources/user/user.service.ts
@@ -5,7 +5,7 @@ import {
 } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import bcrypt from 'bcrypt';
+import * as bcrypt from 'bcrypt';
 
 import { CreateUserDto, UpdateUserDto } from './dto';
 import { IUserService } from './user.interface';
