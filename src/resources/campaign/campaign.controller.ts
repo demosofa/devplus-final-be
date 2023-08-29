e8a3c05dff0ef92fdee24c0c00658368f890c770
demosofa <thientndgcd18746@fpt.edu.vn>
2023-08-29 11:26:32 +0700

    fix: remove auth decorator for testing

--- src/resources/campaign/campaign.controller.ts
+++ src/resources/campaign/campaign.controller.ts
@@ -12,14 +12,16 @@ import { CampaignService } from './campaign.service';
 import { CreateCampaignDto } from './dto/create-campaign.dto';
 import { UpdateCampaignDto } from './dto/update-campaign.dto';
 import { PageOptionsDto } from '../../common/pagination/PageOptionDto';
-import { ReqUser } from '@common/decorators';
+import { Auth, ReqUser } from '@common/decorators';
 import { User } from '@resources/user/entities/user.entity';
+import { ROLE } from '@common/enums';
 
 @Controller('campaign')
 export class CampaignController {
 	constructor(private readonly campaignService: CampaignService) {}
 
 	@Post()
+	@Auth(ROLE.ADMIN)
 	create(@ReqUser() user: User, @Body() createCampaignDto: CreateCampaignDto) {
 		return this.campaignService.create(user, createCampaignDto);
 	}
