69ae1631802176198f4dee04e8596cac270b24ed
VanThen60hz <nguyenvthang2409@gmail.com>
2023-08-30 17:12:53 +0700

    feat: update CV

--- src/resources/cv/cv.service.ts
+++ src/resources/cv/cv.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { CreateCvDto } from './dto/create-cv.dto';
 import { UpdateCvDto } from './dto/update-cv.dto';
 import { PageOptionsDto } from '@common/pagination/PageOptionDto';
@@ -7,12 +7,20 @@ import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { PageMetaDto } from '@common/pagination/PageMetaDto';
 import { PageDto } from '@common/pagination/Page.dto';
+import { CV_STATUS } from '../../common/enums/cv-status';
+import { ROLE, USER_STATUS } from '../../common/enums';
+import { User } from '../user/entities/user.entity';
+import { Role } from '../role/entities/role.entity';
 
 @Injectable()
 export class CvService {
 	constructor(
 		@InjectRepository(Cv)
-		private readonly cvRepos: Repository<Cv>
+		private readonly cvRepos: Repository<Cv>,
+		@InjectRepository(User)
+		private readonly userRepos: Repository<User>,
+		@InjectRepository(Role)
+		private readonly roleRepos: Repository<Role>
 	) {}
 
 	create(createCvDto: CreateCvDto) {
@@ -35,12 +43,30 @@ export class CvService {
 		return new PageDto(entities, pageMetaDto);
 	}
 
-	findOne(id: number) {
+	async findOne(id: number) {
 		return `This action returns a #${id} cv`;
 	}
 
-	update(id: number, updateCvDto: UpdateCvDto) {
-		return `This action updates a #${id} cv`;
+	async update(id: number, updateCvDto: UpdateCvDto) {
+		const oldCV = await this.cvRepos.findOne({
+			where: { id },
+		});
+
+		return this.cvRepos.save({
+			...oldCV,
+			...updateCvDto,
+		});
+	}
+
+	async updateFail(id: number, updateCvDto: UpdateCvDto) {
+		const oldCV = await this.cvRepos.findOne({
+			where: { id },
+		});
+
+		return this.cvRepos.save({
+			...oldCV,
+			...updateCvDto,
+		});
 	}
 
 	remove(id: number) {
