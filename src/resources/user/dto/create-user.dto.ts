01ff56c19f00f59f7bf89e8063ec4eda53213dcf
demosofa <62540039+demosofa@users.noreply.github.com>
2023-09-02 01:02:38 +0700

    feat: create sub account for hr in workspace (#48)

--- src/resources/user/dto/create-user.dto.ts
+++ src/resources/user/dto/create-user.dto.ts
@@ -1,15 +1,8 @@
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
 import { ROLE } from '@common/enums';
 import { Campaign } from '@resources/campaign/entities/campaign.entity';
 import { Workspace } from '@resources/workspace/entities/workspace.entity';
-import {
-	IsArray,
-	IsEmail,
-	IsEnum,
-	IsInstance,
-	IsNotEmpty,
-	IsOptional,
-	IsString,
-} from 'class-validator';
 
 export class CreateUserDto {
 	@IsNotEmpty()
@@ -27,14 +20,9 @@ export class CreateUserDto {
 	@IsString()
 	phone_number: string;
 
-	@IsEnum(ROLE)
 	roleName: ROLE;
 
-	@IsOptional()
-	@IsInstance(Workspace)
 	workspace?: Workspace;
 
-	@IsOptional()
-	@IsArray()
 	campaign?: Campaign[];
 }
