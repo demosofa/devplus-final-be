141ce9e6b68ea255b4ffe5f10091ca396f7f7548
demosofa <62540039+demosofa@users.noreply.github.com>
2023-09-05 18:31:55 +0700

    Hotfix (#54)
    * fix: prepare docker for deployment
    
    * fix: remove auth decorator for testing
    
    * fix: remove auth decorator for testing
    
    * fix: remove auth decorator for testing
    
    * fix: frame ancestors for blocking loading file from server to client

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,24 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DeleteInterceptor } from '@common/interceptors/delete.interceptor';
 import { join } from 'path';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
 	const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-	app.use(helmet({ crossOriginResourcePolicy: { policy: 'cross-origin' } }));
+	const configService = app.get(ConfigService);
+	const CLIENT_URL = configService.get<string>('CLIENT_URL');
+
+	app.use(
+		helmet({
+			crossOriginResourcePolicy: { policy: 'cross-origin' },
+			contentSecurityPolicy: {
+				directives: {
+					'frame-ancestors': ["'self'", CLIENT_URL],
+				},
+			},
+		})
+	);
 	app.enableCors();
 	app.useGlobalInterceptors(new DeleteInterceptor());
 	app.useGlobalPipes(
