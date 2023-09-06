0bbaf1ee72f3c457c22b9250b303f99bf2d245e6
VanThen60hz <nguyenvthang2409@gmail.com>
2023-09-06 15:49:33 +0700

    feat: enable/disable user (#57)

--- src/resources/user/dto/update-user.dto.ts
+++ src/resources/user/dto/update-user.dto.ts
@@ -1,4 +1,10 @@
 import { PartialType } from '@nestjs/mapped-types';
-import { CreateUserDto } from './create-user.dto';
+import { IsOptional } from 'class-validator';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+import { CreateUserDto } from './create-user.dto';
+import { USER_STATUS } from '@common/enums';
+
+export class UpdateUserDto extends PartialType(CreateUserDto) {
+	@IsOptional()
+	status: USER_STATUS;
+}
